f402cb1b7e15f5be9a60165dd504a8df94ea5c56
AngelDimov <angeldimow@abv.bg>
2015-12-14 21:48:46 +0200

    Hero Name and Title

--- Dungeons_and_Python.py
+++ Dungeons_and_Python.py
@@ -1,8 +1,15 @@
-#sdifhgsdfgsddfsgs
+class Hero:
 
-class Hero():
+    def __init__(self, name, title, health, mana, mana_regeneration_rate):
+        self.name = name
+        self.title = title
+        self.healt = health
 
-	def __init__(self):
-		pass
+    def known_as(self):
+        return "{} the {}".format(self.name, self.title)
+
+h = Hero(name="Bron", title="Dragonslayer", health=100,
+         mana=100, mana_regeneration_rate=2)
+
+	def get_health(self):
 		
-	
