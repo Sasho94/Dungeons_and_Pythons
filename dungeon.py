32999acd7deba214d287dc0446fcedd7c144127d
Sasho94 <alexandermladjov@abv.bg>
2015-12-16 21:27:01 +0200

    map

--- dungeon.py
+++ dungeon.py
@@ -1,11 +1,29 @@
 class Dungeon:
     FMAP = ""
+
     def __init__(self, filemap):
         self.FMAP = filemap
 
     def print_map(self):
-        file = open(self.FMAP, "r")
-        for line in file:
-            print(line)
+        f = open(self.FMAP, "r")
+        print f.read()
+        f.close
+
+    def move_hero(self,direction):
+        jarr = []
+        iarr = []
+        with open(self.FMAP) as f:
+            while True:
+                c = f.read(1)
+                if not c:
+                    print "End of file"
+                    break
+                elif c=='n':
+                    iarr.append[jarr[]]
+                else:
+                    jarr.append[c]
+        print jarr
+
 map = Dungeon("level1.txt")
 map.print_map()
+map.move_hero("as")
